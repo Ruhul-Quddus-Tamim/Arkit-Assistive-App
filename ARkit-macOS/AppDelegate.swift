10a1faec8a4f83a533d6465ab656690ed57af03d
Tamim <86849949+Ruhul-Quddus-Tamim@users.noreply.github.com>
2026-01-26 11:05:23 +0800

    added AppDelegate.swift

---
+++ ARkit-macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+import AppKit
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.title = "Eye Tracking App"
+        
+        let viewController = ViewController()
+        window.contentViewController = viewController
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
+
